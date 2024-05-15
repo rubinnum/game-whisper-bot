c2cc29f57174d57c0edb3a39c2ef00e9e03d7705
Stingzzy <ismailabedin5@gmail.com>
2024-05-15 20:40:33 +0200

    Basic bot interaction

---
+++ bot/bot_interaction.py
@@ -0,0 +1,27 @@
+from typing import Final
+
+import telebot
+from telebot import types
+
+from constants import BOT_TOKEN
+
+bot = telebot.TeleBot(BOT_TOKEN, parse_mode=None)
+BOT_USERNAME: Final = '@game_whisper_bot'
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, ' Welcome to Gaming Whisper')
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    button_1 = types.InlineKeyboardButton('Game Profile')
+    button_2 = types.InlineKeyboardButton('Personal Recommendation')
+    markup.add(button_1, button_2)
+    bot.send_message(message.chat.id, "Your choice for today:", reply_markup=markup)
+
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
