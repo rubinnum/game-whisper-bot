87c355ed939cee828a934a1cc5a188c0cdc0f77b
rubinnum <romarypiak@gmail.com>
2024-05-15 18:49:23 +0200

    ChatGPT functionality integrated

--- bot/get_recommendations_list.py
+++ bot/get_recommendations_list.py
@@ -1,15 +1,34 @@
-import openai
-from constants import OPENAI_API_KEY
+from openai import OpenAI
 
-openai.api_key = OPENAI_API_KEY
+import constants
+
+client = OpenAI(organization=constants.ORGANIZATION_ID,
+                project=constants.PROJECT_ID,
+                api_key=constants.OPENAI_API_KEY)
 
 
-def get_recommended_games(favourite_games):
-    pass
+def get_recommended_games(preferences):
+    boilerplate_content = ('Based on the array of video games that I will provide you with, recommend me five video '
+                           'games that I might like (only names, no explanation) divided with a coma\n')
+
+    response = client.chat.completions.create(
+        model=constants.GPT_MODEL,
+        messages=[
+            {'role': 'user', 'content': boilerplate_content + str(preferences)}
+        ]
+    )
+    assistant_response = response.choices[0].message.content.strip()
+    recommendation_list = assistant_response.split(', ')
+    return recommendation_list
+
+
+def output_recommended_games(preferences):
+    recommended_games = get_recommended_games(preferences)
+    print('Here is the list of recommended games: ')
+    for i in range(len(recommended_games)):
+        print(f'{i + 1}. {recommended_games[i]}')
 
 
 if __name__ == '__main__':
-    preferences = input('What are the games that you like (divide them with newline): ')
-    favourite_games = preferences.split('\n')
-    favourite_games = [game.strip() for game in favourite_games]
-    get_recommended_games(favourite_games)
+    preferences = input('Enter your favourite games (separate by comma and space): ').split(', ')
+    output_recommended_games(preferences)
