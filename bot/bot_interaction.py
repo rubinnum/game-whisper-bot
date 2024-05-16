47f15904c5a457ce3f777b6aa44ad60f35b7f77e
Stingzzy <ismailabedin5@gmail.com>
2024-05-17 00:47:26 +0200

    Minor improvements

--- bot/bot_interaction.py
+++ bot/bot_interaction.py
@@ -5,23 +5,23 @@ from telebot import types
 
 from constants import BOT_TOKEN
 
-bot = telebot.TeleBot(BOT_TOKEN, parse_mode=None)
-BOT_USERNAME: Final = '@game_whisper_bot'
+bot = telebot.TeleBot(BOT_TOKEN)
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['hi', 'start'])
 def send_welcome(message):
     bot.reply_to(message, ' Welcome to Gaming Whisper')
     markup = types.ReplyKeyboardMarkup(row_width=2)
-    button_1 = types.InlineKeyboardButton('Game Profile')
-    button_2 = types.InlineKeyboardButton('Personal Recommendation')
-    markup.add(button_1, button_2)
+    game_profile_button = types.InlineKeyboardButton('Game Profile')
+    recommendation_button = types.InlineKeyboardButton('Personal Recommendation')
+    markup.add(game_profile_button, recommendation_button)
     bot.send_message(message.chat.id, "Your choice for today:", reply_markup=markup)
 
 
-@bot.message_handler(func=lambda m: True)
+@bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
 
 
-bot.infinity_polling()
+if __name__ == '__main__':
+    bot.infinity_polling()
